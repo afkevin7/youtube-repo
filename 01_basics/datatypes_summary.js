057ceceadc1f54585a31bf7e858a3a0cac0eb242
afkevin7 <121594791+afkevin7@users.noreply.github.com>
2024-01-26 06:57:17 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// Primitive (Call by Value)
+
+// 7 Types: String, Number, Boolean, BigInt, Symbol, null, Undefined
+
+// const score = 100
+// const scoreValue = 100.2
+
+// const isLoggedIn = true
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol("123")
+// const numberId = Symbol("123")
+
+// console.log( id === numberId)
+
+// const bigNumber = 21637643547472334n
+
+// Reference (Call By Reference)
+
+// Array, Objects, Functions
+
+const heros = ["batman", "superman", "aquaman"];
+let myObj = {
+    name: "Kevin",
+    age: 24,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log("Hello");
